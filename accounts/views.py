722a1128c737c467d3ea56f7434d28c1816af812
Michael Kimani <kimanimyke@gmail.com>
2025-07-08 14:54:44 +0300

    Dynamically Redirect the User to the Next Page

--- accounts/views.py
+++ accounts/views.py
@@ -12,6 +12,7 @@ from django.core.mail import EmailMessage
 from django.http import HttpResponse
 from carts.models import Cart, CartItem
 from carts.views import _cart_id
+import requests
 
 # Create your views here.
 def register(request):
@@ -100,7 +101,15 @@ def login(request):
                 pass
             auth.login(request, user)
             messages.success(request, "You are logged in.")
-            return redirect('dashboard')
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
         else:
             messages.error(request, 'Invalid login credentials.')
             return redirect('login')
