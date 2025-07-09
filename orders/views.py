746f9a55a27830d23cb147d34c0ea14906100bf9
Michael Kimani <kimanimyke@gmail.com>
2025-07-09 14:07:02 +0300

    payment page

--- orders/views.py
+++ orders/views.py
@@ -6,6 +6,10 @@ from .models import Order
 import datetime
 
 # Create your views here.
+def payments(request):
+    return render(request, 'orders/payments.html')
+
+
 def place_order(request, total=0, quantity=0):
     current_user = request.user
 
@@ -53,6 +57,15 @@ def place_order(request, total=0, quantity=0):
             data.order_number = order_number
             data.save()
 
-            return redirect('checkout')
+            order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
+            context = {
+                'order': order,
+                'cart_items': cart_items,
+                'total': total,
+                'tax': tax,
+                'grand_total': grand_total,
+            }
+
+            return render(request, 'orders/payments.html', context)
     else:
         return redirect('checkout')
