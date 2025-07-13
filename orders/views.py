7ba3186f9bf35a853aaf4bc0e997db714d77ff26
Michael Kimani <kimanimyke@gmail.com>
2025-07-13 16:05:36 +0300

    Order Completion Invoice

--- orders/views.py
+++ orders/views.py
@@ -139,10 +139,20 @@ def order_complete(request):
     transID = request.GET.get('payment_id')
     try:
         order = Order.objects.get(order_number=order_number, is_ordered=True)
-        order_products = OrderProduct.objects.filter(order_id=order.id)
+        ordered_products = OrderProduct.objects.filter(order_id=order.id)
+
+        subtotal = 0
+        for i in ordered_products:
+            subtotal += i.product_price * i.quantity
+
+        payment = Payment.objects.get(payment_id=transID)
         context = {
             'order': order,
-            'order_products': order_products,
+            'ordered_products': ordered_products,
+            'order_number': order.order_number,
+            'transID': payment.payment_id,
+            'payment': payment,
+            'subtotal': subtotal
         }
         return render(request, 'orders/order_complete.html', context)
     except (Payment.DoesNotExist, Order.DoesNotExist):
