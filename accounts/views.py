28aa10a1fe70a5dea1b380eed54af3bcab885666
Michael Kimani <kimanimyke@gmail.com>
2025-07-16 16:31:55 +0300

    Order Detail Dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -13,7 +13,7 @@ from django.http import HttpResponse
 from carts.models import Cart, CartItem
 from carts.views import _cart_id
 import requests
-from orders.models import Order
+from orders.models import Order, OrderProduct
 
 # Create your views here.
 def register(request):
@@ -268,3 +268,17 @@ def change_password(request):
             return redirect('change_password')
 
     return render(request, 'accounts/change_password.html')
+
+@login_required(login_url='login')
+def order_detail(request, order_id):
+    order_detail = OrderProduct.objects.filter(order__order_number=order_id)
+    order = Order.objects.get(order_number=order_id)
+    subtotal = 0
+    for i in order_detail:
+        subtotal = i.product_price * i.quantity
+    context = {
+        'order_detail': order_detail,
+        'order': order,
+        'subtotal': subtotal,
+    }
+    return render(request, 'accounts/order_detail.html', context)
